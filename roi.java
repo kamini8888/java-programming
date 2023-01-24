ea158bb876f3542a011cdb7ca6ff1508cd7b048a
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-01-24 18:19:32 +0530

    Create roi.java

---
+++ roi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class roi
+{
+    public static void main(String[] args)
+    {
+        int p,t,r;
+	    float si;
+	    String s;
+        Scanner sc=new Scanner(System.in);
+	    System.out.println("Senior citizen(yes/no): ");
+	    s=sc.nextLine();
+		System.out.print("Enter the principle ammount: ");
+		p=sc.nextInt();
+		System.out.print("Enter the time period: ");
+		t=sc.nextInt();
+		if(s.equals("yes"))
+	    {
+	        r=12;
+	        si=(p*t*r)/100;
+	        System.out.println("Simple interest= "+si);
+	    }
+	    else if(s.equals("no"))
+	    {
+	        r=10;
+	        si=(p*t*r)/100;
+	        System.out.println("Simple interest= "+si);
+	    }
+	    else
+	    {
+	        System.out.println("Invalid");
+	    }
+    }
+	
+}
