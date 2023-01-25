5e3e899b479fbd0e2ab0f98dde9838af16178fb2
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-01-25 12:00:48 +0530

    Create cnvt.java

---
+++ cnvt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class cnvt{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("enter a string");
+String str=s.nextLine();
+int a=Integer.parseInt(str);
+System.out.println("string to integer is"+a);
+}
+}
