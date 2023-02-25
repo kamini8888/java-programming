6f29ef0078a35bf0cffd674599c780c732d2ba82
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-02-25 20:20:19 +0530

    Create salary.java

---
+++ salary.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class salary{
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the grade of the employee");
+char ch = in.next().charAt(0);
+if (ch == 'A') {
+System.out.println("Enter the employee salary");
+int a = in.nextInt();
+if (a < 10000) {
+int bonus = a * 7/100;
+int total = a + bonus;
+System.out.println("BONUS= " + bonus);
+System.out.println("Total to be paid=" + total);
+} 
+else {
+int bonus = a * 5/100;
+int total = a + bonus;
+System.out.println("BONUS= " + bonus);
+System.out.println("Total to be paid=" + total);
+}
+}
+else if (ch == 'B') {
+System.out.println("Enter the employee salary");
+int b = in.nextInt();
+if (b < 10000) {
+int bonus = b * 12/100;
+int total = b + bonus;
+System.out.println("BONUS=" + bonus);
+System.out.println("Total to be paid=" + total);
+}
+else {
+int bonus = b * 10/100;
+int total = b + bonus;
+System.out.println("BONUS=" + bonus);
+System.out.println("Total to be paid=" + total);
+}
+}
+}
+}
