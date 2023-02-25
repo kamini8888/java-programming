d794dd9e2bf4209e85f83d3c3e94d2000c2dd784
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-02-25 20:19:28 +0530

    Create posi.java

---
+++ posi.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class posi
+{
+  private static Scanner sc;
+  public static void main(String[] args) 
+  {
+    int Size, i;
+    sc = new Scanner(System.in);
+    System.out.print(" Please Enter Number of elements in an array : ");
+    Size = sc.nextInt();  
+    int [] a = new int[Size];
+    System.out.print(" Please Enter " + Size + " elements of an Array  : ");
+    for (i = 0; i < Size; i++)
+    {
+      a[i] = sc.nextInt();
+    }   
+
+    for(i = 0; i < Size; i++)
+    {
+      if(a[i] >= 0)
+      {
+        System.out.println("Positive Number:"+a[i]);
+      }
+      else
+      {
+        System.out.println("Negative Number:"+a[i]);
+      }
+    }  
+  }
+}
