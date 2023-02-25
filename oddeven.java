495cead1f070d19fe8835c874a3aad72a74d5a77
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-02-25 20:14:11 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class odd
+{
+public static void main(String[] args) 
+{
+int x;
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter: ");
+x=sc.nextInt();
+if(x%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
