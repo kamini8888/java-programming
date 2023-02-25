842dd227ff009f44c07c679ce371e77a03c0d32f
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2023-02-25 20:38:42 +0530

    Create ari.java

---
+++ ari.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ari
+{
+public static void main(String args[])
+{
+int a,b,c,d,e,f;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+System.out.println("Sum ="+c);
+System.out.println("difference="+d);
+System.out.println("product="+e);
+System.out.println("ratio="+f);
+}
+}
